8d6209027bd5c698b5ef41686c3f3e5df3fd8028
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-05 17:21:19 +0700

    83. Google Sign In Authentication 2

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-// const config = {
-//   apiKey: "AIzaSyBX7tLWYLz4do2leigpf1lzmfdFeI0gU28",
-//   authDomain: "crwn-db-4184b.firebaseapp.com",
-//   databaseURL: "https://crwn-db-4184b.firebaseio.com",
-//   projectId: "crwn-db-4184b",
-//   storageBucket: "crwn-db-4184b.appspot.com",
-//   messagingSenderId: "223738044700",
-//   appId: "1:223738044700:web:f9de474162462ab09e9ad9",
-//   measurementId: "G-YJWYN3HP1N"
-// };
+const config = {
+  apiKey: "AIzaSyBX7tLWYLz4do2leigpf1lzmfdFeI0gU28",
+  authDomain: "crwn-db-4184b.firebaseapp.com",
+  databaseURL: "https://crwn-db-4184b.firebaseio.com",
+  projectId: "crwn-db-4184b",
+  storageBucket: "crwn-db-4184b.appspot.com",
+  messagingSenderId: "223738044700",
+  appId: "1:223738044700:web:f9de474162462ab09e9ad9",
+  measurementId: "G-YJWYN3HP1N"
+};
 
 firebase.initializeApp(config);
 
