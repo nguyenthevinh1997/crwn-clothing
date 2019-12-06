40bc0bc95bd5ec75ad9965ef0a3b7ebb6d25232a
The Vinh <nguyenthevinh1997@gmail.com>
2019-12-06 11:12:32 +0700

    92. Sign In With Email and Password
    Finishing sign in handleSubmit

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,7 +19,7 @@ const Header = ({ currentUser }) => (
       </Link>
 
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut}>
+        <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
