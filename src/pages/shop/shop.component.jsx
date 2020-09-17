8e40e99b940813f2bbb7cf3fa11b1e0ed7fc8d16
The Vinh <vinhnt02@ominext.com>
2020-09-17 10:04:46 +0700

    122. updating our application with reselect, replaced cart selectors with memoized selectors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,23 +3,23 @@ import SHOP_DATA from './shop.data.js';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component{
-    constructor(props){
+class ShopPage extends React.Component {
+    constructor(props) {
         super(props);
 
         this.state = {
             collections: SHOP_DATA
         }
     }
-    render(){
-        const {collections} = this.state;
+    render() {
+        const { collections } = this.state;
         return (<div className="shop-page">
-        {
-            collections.map(({id,...otherCollectionProps})=>(
-                <CollectionPreview key={id}{...otherCollectionProps}/>
-            ))
-        }
-            </div>);
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id}{...otherCollectionProps} />
+                ))
+            }
+        </div>);
     }
 }
 
